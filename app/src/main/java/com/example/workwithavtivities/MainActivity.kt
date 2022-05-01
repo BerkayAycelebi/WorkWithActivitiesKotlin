4331de27f9f00d4f4e1bcded1e174c5af27f428c
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2022-05-01 10:00:35 +0300

    Aktivitler arası veri aktarımı

--- app/src/main/java/com/example/workwithavtivities/MainActivity.kt
+++ app/src/main/java/com/example/workwithavtivities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +14,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun PassActivity2(view : View)
-    {
+    {   val data = txtData.text.toString()
         val intent=Intent(applicationContext,SeconActivity::class.java)
+        intent.putExtra("data",data)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/workwithavtivities/SeconActivity.kt
+++ app/src/main/java/com/example/workwithavtivities/SeconActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_secon.*
+
 
 class SeconActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secon)
+
+        val i=intent
+        val getData=i.getStringExtra("data")
+        textView.text=getData
     }
 
-    fun PassActivity2(view:View)
-    {
-        val i= Intent(applicationContext,MainActivity::class.java)
-        startActivity(i)
-    }
 }
